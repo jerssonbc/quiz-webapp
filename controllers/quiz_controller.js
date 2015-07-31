b419e2fb611b3ccd801cdd9637d16780e2f61933
Jersson <jersf_bc_10@hotmail.com>
2015-07-30 22:02:48 -0500

    Validación de entradas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -24,13 +24,13 @@ exports.index=function(req,res){
       models.Quiz.findAll({where:["pregunta like ? ",'%'+texto_a_buscar+'%'],
                           order:'pregunta ASC'}).then(
         function(quizes){
-          res.render('quizes/index.ejs',{quizes:quizes});
+          res.render('quizes/index.ejs',{quizes:quizes,errors:[]});
         }
       ).catch(function(error){next(error);})
   }else{
       models.Quiz.findAll().then(
         function(quizes){
-          res.render('quizes/index.ejs',{quizes:quizes});
+          res.render('quizes/index.ejs',{quizes:quizes,errors:[]});
         }
       ).catch(function(error){next(error);})
   }
@@ -39,7 +39,7 @@ exports.index=function(req,res){
 
 // GET /quizes/:id
 exports.show=function(req,res){
-    res.render('quizes/show',{quiz:req.quiz});
+    res.render('quizes/show',{quiz:req.quiz,errors:[]});
 };
 
 // GET /quizes/:id/answer
@@ -50,7 +50,7 @@ exports.answer=function(req,res){
       resultado='Correcto'
   }
   res.render('quizes/answer',
-              {quiz:req.quiz, respuesta:resultado});
+              {quiz:req.quiz, respuesta:resultado,errors:[]});
 };
 
 //GET /quizes/new
@@ -58,15 +58,27 @@ exports.new=function(req,res){
     var quiz=models.Quiz.build( //crea objeto quiz
       {pregunta:"Pregunta",respuesta:"Respuesta"}
     );
-    res.render('quizes/new',{quiz:quiz});
+    res.render('quizes/new',{quiz:quiz,errors:[]});
 };
 
 // POST /quizes/create
 
 exports.create=function(req,res){
   var quiz=models.Quiz.build(req.body.quiz);
-  // guarda en la DB los campos pregutn y respuesta de quiz
-  quiz.save({fields:["pregunta","respuesta"]}).then(function(){
-    res.redirect('/quizes');
-  })
+  var errors=quiz.validate();
+  if(errors){
+    var i=0;
+    var errores=new Array();
+    //se convierte en [] con la propiedad message por compatibilida con layout
+    for(var prop in errors){
+      errores[i++]={message:errors[prop]};
+    }
+    res.render('quizes/new',{quiz:quiz,errors:errores});
+  }else{
+    // guarda en la DB los campos pregutn y respuesta de quiz
+    quiz.save({fields:["pregunta","respuesta"]}).then(function(){
+      res.redirect('/quizes');
+      // res.reditect :Redireccioin HTTP a lista de preguntas
+    })
+    }
 };
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var quizController=require('../controllers/quiz_controller');
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz',errors:[] });
 });
 
 //Autoload de comandos con :quizId
@@ -17,7 +17,7 @@ router.get('/quizes/new',quizController.new);
 router.post('/quizes/create',quizController.create);
 
 router.get('/author',function(req,res){
-  res.render('author');
+  res.render('author',{errors:[]});
 });
 
 module.exports = router;

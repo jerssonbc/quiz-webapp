9ed408b8def0ee49ac95fa3962eedde8773af3fa
Jersson <jersf_bc_10@hotmail.com>
2015-08-13 15:52:22 -0500

    Moderación de Comentarios

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -1,5 +1,25 @@
 var models=require('../models/models.js');
 
+// Autoload :id de comentarios
+
+exports.load=function(req,res,next,commentId){
+  models.Comment.find({
+            where:{
+                id:Number(commentId)
+            }
+        }).then(function(comment){
+          if(comment){
+            req.comment=comment;
+            next();
+          }else{
+            next(new Error('No existe commentId = '+commentId))
+          }
+        }).catch(function(error){
+          next(error);
+        });
+};
+
+
 //GET /quizes/:quizId/comments/new
 
 exports.new=function(req,res){
@@ -27,7 +47,7 @@ exports.create=function(req,res){
       res.render('comments/new.ejs',
                   {comment:comment,quizId:req.params.quizId,errors:errores});
     }else{
-      // guarda en la DB  campo texto de commment
+      // guarda en la DB  campo texto de comment
       comment.save().then(function(){
         res.redirect('/quizes/'+req.params.quizId);
         // res.reditect :Redireccioin HTTP a lista de preguntas
@@ -35,4 +55,17 @@ exports.create=function(req,res){
         next(error);
       });
       }
+};
+
+// GET /quizes/:quizId/comments/ : commentId/publish
+
+exports.publish=function(req,res){
+  req.comment.publicado=true;
+
+  req.comment.save({fields:["publicado"]})
+    .then(function(){
+      res.redirect('/quizes/'+req.params.quizId);
+    }).catch(function(error){
+      next(error);
+    });
 };
--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ router.get('/', function(req, res) {
 
 //Autoload de comandos con :quizId
 router.param('quizId',quizController.load); //autoload :quizId
+router.param('commentId',commentController.load);//autoload: commentID
 
 //Definicion de rutas session
 router.get('/login',sessionController.new);//formulario login
@@ -31,7 +32,8 @@ router.delete('/quizes/:quizId(\\d+)',sessionController.loginRequired, quizContr
 
 router.get('/quizes/:quizId(\\d+)/comments/new',commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',commentController.create);
-
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',
+                                        sessionController.loginRequired,commentController.publish);;
 
 router.get('/author',function(req,res){
   res.render('author',{errors:[]});
